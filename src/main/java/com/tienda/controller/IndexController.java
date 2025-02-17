c246acaea17a936cbbe703f0830440de9aa67bf6
William Bastos <william30bastos@gmail.com>
2025-02-16 20:53:10 -0600

    Semana 04 "reposicion"

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        //model.addAttribute("attribute", "value");
+        return "index";
+    }
+    
+    @RequestMapping("/informacion")
+    public String contacto(Model model) {
+        return "contacto";
+    }
+
+}
+ 
