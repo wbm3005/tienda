659a91f3357c502770d3b90871f0646afb44f649
William Bastos <william30bastos@gmail.com>
2025-02-20 02:29:00 -0600

    Semana 06

--- src/main/java/com/tienda/controller/CategoriaController.java
+++ src/main/java/com/tienda/controller/CategoriaController.java
@@ -1,10 +1,16 @@
 package com.tienda.controller;
 
+import com.tienda.domain.Categoria;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.tienda.service.CategoriaService;
+import com.tienda.service.impl.FirebaseStorageServiceImpl;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/categoria")
@@ -13,6 +19,8 @@ public class CategoriaController {
     @Autowired
     private CategoriaService categoriaService;
     
+    @Autowired
+    private FirebaseStorageServiceImpl firebaseStorageService;
     
     @RequestMapping("/listado")
     public String page(Model model) {
@@ -20,6 +28,34 @@ public class CategoriaController {
         model.addAttribute("categorias", categorias);
         model.addAttribute("totalCategorias", categorias.size());
         return "categoria/listado";
+    }
+    
+    @PostMapping("/guardar")
+    public String categoriaGuardar(Categoria categoria,
+            @RequestParam("imagenFile") MultipartFile imagenFile) {        
+        if (!imagenFile.isEmpty()) {
+            categoriaService.save(categoria);
+            categoria.setRutaImagen(
+                    firebaseStorageService.cargaImagen(
+                            imagenFile, 
+                            "categoria", 
+                            categoria.getIdCategoria()));
+        }
+        categoriaService.save(categoria);
+        return "redirect:/categoria/listado";
+    }
+    
+    @GetMapping("/eliminar/{idCategoria}")
+    public String categoriaEliminar(Categoria categoria) {
+        categoriaService.delete(categoria);
+        return "redirect:/categoria/listado";
+    }
+
+    @GetMapping("/modificar/{idCategoria}")
+    public String categoriaModificar(Categoria categoria, Model model) {
+        categoria = categoriaService.getCategoria(categoria);
+        model.addAttribute("categoria", categoria);
+        return "/categoria/modifica";
     }
 
 }
--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -14,14 +14,33 @@ public class CategoriaServiceImpl implements CategoriaService {
 
     @Autowired
     private CategoriaDao categoriaDao;
+
+    @Override
+    @Transactional(readOnly = true)
     public List<Categoria> getCategorias(boolean activos) {
         var lista = categoriaDao.findAll();
-        
         if (activos) {
-            lista.removeIf(c -> c.isActivo());
+            lista.removeIf(e -> !e.isActivo());
         }
-        
         return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.deleteById(categoria.getIdCategoria());
     }
     
 }
